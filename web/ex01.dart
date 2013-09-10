06afd838595c952d635ccad6b2abf504ac1690d2
Dzenan Ridjanovic <dzenanr@gmail.com>
2013-09-10 11:24:37 -0400

    add doc

--- web/ex01.dart
+++ web/ex01.dart
@@ -1,25 +1,16 @@
-
-
 num x = 0;
 String z;
 num a;
 
-
-
-
 void add(){
-  
-  x=x*10+1;
+  x = x * 10 + 1;
 }
 
 void main() {
   for (var i = 0; i < 9; i++) {
     add();
-     a = x*x;
-     z = x.toString()+ "x" +x.toString() + '='  + a.toString();
+    a = x * x;
+    z = x.toString() + "x" + x.toString() + '=' + a.toString();
     print(z);
-  
   }
-  
-  
 }
